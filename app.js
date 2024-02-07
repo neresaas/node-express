12a9ad0c99943de98975f6ccbddea85a92a0e53e
Nerea <neresaas@gmail.com>
2024-02-07 21:49:03 +0100

    basic express sts (Back-end)\05 Definicructure

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+let app = express();
+
+app.listen(8081, () => {
+    console.log('Server in 8081')
+});
+
+// Para enviarlo al repositorio
+// git add . ----> Detecta todos los cambios que ha habido en el proyecto desde la última vez que hemos salvado los cambios en el repositorio
+// git commit -m " " ----> Poner un nombre a todos los cambios efectuados
+// git push ----> Envía el código del repositorio local al repositorio distribuido de Github
